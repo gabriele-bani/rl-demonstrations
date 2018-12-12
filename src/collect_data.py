f61a93cecab2d49d681763a6e8354e3a4b597e4f
askliar <andreyws96@gmail.com>
2018-12-12 17:08:55 +0100

    Add training from scratch into data collection

--- src/collect_data.py
+++ src/collect_data.py
@@ -52,15 +52,11 @@ eps_lst = [0, 10, 50]
 smoothing_num = 20
 stop_coeff = 5
 
-eps_iterations = 1
-intial_eps = 0.5
-final_eps = 0.05
-
-
 random.seed(seed)
 torch.manual_seed(seed)
 np.random.seed(seed)
 env = gym.envs.make(env_name)
+env.reset()
 
 env.seed(seed)
 
@@ -78,10 +74,10 @@ for index, row in data.iterrows():
             for i in range(num_datapoints):
                 testing_seed = np.random.randint(0, 5000)
                 print(
-                    f"Starting Training with eps={eps}, num_splits={split}, row={index}, seed={testing_seed}, {i}-th run")
+                    f"Starting Backward Training with eps={eps}, num_splits={split}, row={index}, seed={testing_seed}, {i}-th run")
                 trajectory = row.trajectory
                 seed = row.seed
-                returns = row.sum_reward
+                demonstration_value = row.sum_reward
                 get_epsilon = lambda it: intial_eps - it*((intial_eps - final_eps)/eps_iterations) \
                                         if it < eps_iterations \
                                         else final_eps
@@ -105,24 +101,43 @@ for index, row in data.iterrows():
                     testing_seed=testing_seed
                 )
                 results.append((
-                    env_name,
                     returns_trends,
                     testing_seed,
-                    sum_reward,
+                    demonstration_value,
                     split,
                     eps,
                     stop_coeff,
                     smoothing_num
                 ))
-                # row["env_name"] = env_name
-                # row["env_params"] = env_params
-                # row["returns"] = returns
-                # row["seed"] = seed
-                # row["demonstration_value"] = demonstration_value
-                # row["chunks"] = chunks
-                # row["eps_iterations"] = eps_iterations
-                # row["stop_victories"] = stop_victories
-                # row["smoothing_victories"] = smoothing_victories
-                # row["train_length"] = len(returns)
-                # row["time"] = time
+
+eps_iterations = 1
+intial_eps = 0.5
+final_eps = 0.05
+
+random.seed(seed)
+torch.manual_seed(seed)
+np.random.seed(seed)
+env.reset()
+
+env.seed(seed)
+
+for i in range(num_datapoints):
+                testing_seed = np.random.randint(0, 5000)
+                print(
+                    f"Starting Training from scratch with eps={eps}, num_splits={split}, row={index}, seed={testing_seed}, {i}-th run")
+                get_epsilon = lambda it: return 1 - it*((1 - final_eps)/eps_iterations) if it < eps_iterations else final_eps
+                episode_durations, rewards, disc_rewards, losses, trajectories = run_episodes(train_QNet_true_gradient, model, memory,
+                                                                            env, num_episodes, batch_size, discount_factor, learn_rate, 
+                                                                            get_epsilon=get_epsilon, use_target_qnet=use_target_qnet, 
+                                                                            render=render)
+                results.append((
+                    rewards,
+                    testing_seed,
+                    None,
+                    None,
+                    None,
+                    None,
+                    None
+                ))
+
 utils.store_experiments(env_name, None, results)
